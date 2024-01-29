f28770ff13d31b5523d62ed46e7808f8114d1667
Orflo <lual.antonelli@gmail.com>
2024-01-29 18:02:41 +0100

    v2

--- ProyectoNodeJS8/VC/config.js
+++ ProyectoNodeJS8/VC/config.js
@@ -1,4 +1,4 @@
-export const PORT = process.env.PORT || 7761
+export const PORT = process.env.PORT || 3000
 export const DB_HOST = process.env.DB_HOST || 'monorail.proxy.rlwy.net'
 export const DB_NAME = process.env.DB_NAME || 'railway'
 export const DB_USER = process.env.DB_USER || 'root'
